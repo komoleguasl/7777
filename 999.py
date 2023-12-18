2082f7d0be033ea50f0aee3b9d57a8fdd0eb515e
Prokuratura <antitila322@gmail.com>
2023-12-18 19:00:51 +0200

    Init

---
+++ 999.py
@@ -0,0 +1,4 @@
+a = int(input("Введіть значення:"))
+b = int(input("Введіть відсоток:"))
+print("Hello guys")
+print("Результат:", b / 100 * a)
---
+++ main.py
@@ -0,0 +1,5 @@
+pi = 3.14
+r = int(input("Введіть радіус кола: "))
+print("Hello guys")
+print("Довжина кола:",2 * pi * r)
+print("Площа кола:",pi * r ** 2)
